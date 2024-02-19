06433d85e433cb814ee36c255ac2ff6dbee4a416
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2024-02-19 17:41:08 +1100

    Add files via upload

---
+++ 00_scripts/01_quality_check_by_FastQC.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# installing FastQC from https://www.bioinformatics.babraham.ac.uk/projects/download.html
+# FastQC v0.11.9 (Mac DMG image)
+
+# Correct tool citation : Andrews, S. (2010). FastQC: a quality control tool for high throughput sequence data.
+
+
+## WORKING_DIRECTORY=/home/fungi/MADCAPS_NGS/01_raw_data/Original_reads_TUFA/Reads
+## OUTPUT=/home/fungi/MADCAPS_NGS/02_quality_check/Original_reads_TUFA
+## 
+## # Make the directory (mkdir) only if not existe already(-p)
+## mkdir -p $OUTPUT
+## 
+## eval "$(conda shell.bash hook)"
+## conda activate fastqc
+## 
+## cd $WORKING_DIRECTORY
+## 
+## for FILE in $(ls $WORKING_DIRECTORY/*.fastq)
+## do
+##       fastqc $FILE -o $OUTPUT
+## done ;
+## 
+## 
+## conda deactivate fastqc
+## conda activate multiqc
+## 
+## # Run multiqc for quality summary
+## 
+## multiqc $OUTPUT
+
+WORKING_DIRECTORY=/home/fungi/MADCAPS_NGS/01_raw_data/Original_reads_16S_ITS_18S
+OUTPUT=/home/fungi/MADCAPS_NGS/02_quality_check/Original_reads_16S_ITS_18S
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p $OUTPUT
+
+eval "$(conda shell.bash hook)"
+conda activate fastqc
+
+cd $WORKING_DIRECTORY
+
+for FILE in $(ls $WORKING_DIRECTORY/*.fastq)
+do
+      fastqc $FILE -o $OUTPUT
+done ;
+
+
+conda deactivate fastqc
+conda activate multiqc
+
+# Run multiqc for quality summary
+
+multiqc $OUTPUT
+
