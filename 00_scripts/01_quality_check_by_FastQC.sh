20d305ae309507e803629718fe4fb739f3d8b4c5
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2024-02-19 22:31:33 +1300

    Update 01_quality_check_by_FastQC.sh

--- 00_scripts/01_quality_check_by_FastQC.sh
+++ 00_scripts/01_quality_check_by_FastQC.sh
@@ -17,7 +17,7 @@
 ## 
 ## cd $WORKING_DIRECTORY
 ## 
-## for FILE in $(ls $WORKING_DIRECTORY/*.fastq)
+## for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
 ## do
 ##       fastqc $FILE -o $OUTPUT
 ## done ;
@@ -41,7 +41,7 @@ conda activate fastqc
 
 cd $WORKING_DIRECTORY
 
-for FILE in $(ls $WORKING_DIRECTORY/*.fastq)
+for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
 do
       fastqc $FILE -o $OUTPUT
 done ;
